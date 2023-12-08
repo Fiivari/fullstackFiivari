65c72c1b3b37053a9366d5218f148d36f1f2caba
Fiivari <126698151+Fiivari@users.noreply.github.com>
2023-12-08 18:56:51 +0200

    part1 exercises

---
+++ osa1/1.6/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => {
+    setGood(good + 1)
+  }
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const addBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={addGood} text='good' />
+      <Button handleClick={addNeutral} text='neutral' />
+      <Button handleClick={addBad} text='bad' />
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
+export default App
