1ea914dbc6e2ffaeabdaa9d4313f353c97aaba9c
Fiivari <126698151+Fiivari@users.noreply.github.com>
2023-12-08 19:37:08 +0200

    ex 1.11

---
+++ osa1/1.11/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticsLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+  
+)
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good + neutral + bad === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticsLine text='good' value={good} />
+        <StatisticsLine text='neutral' value={neutral} />
+        <StatisticsLine text='bad' value={bad} />
+        <StatisticsLine text='all' value={good + neutral + bad} />
+        <StatisticsLine text='average' value={(good - bad) / 
+          (good + neutral + bad)} />
+        <StatisticsLine text='positive' value={100 * good / 
+          (good + neutral + bad) + ' %'} />
+      </tbody>
+    </table> 
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => {
+    setGood(good + 1)
+  }
+
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const addBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={addGood} text='good' />
+      <Button handleClick={addNeutral} text='neutral' />
+      <Button handleClick={addBad} text='bad' />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
