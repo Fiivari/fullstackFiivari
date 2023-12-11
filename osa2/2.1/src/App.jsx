d9bafdcecba9cdd4cdba10540da6c1ca7b365f71
Fiivari <126698151+Fiivari@users.noreply.github.com>
2023-12-11 20:25:30 +0200

    added osa2

---
+++ osa2/2.1/src/App.jsx
@@ -0,0 +1,75 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>{part.name} {part.exercises}</p>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+// const Total = (props) => {
+//   return (
+//     <div>
+//       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises
+//        + props.parts[2].exercises}</p>
+//     </div>
+//   )
+// }
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
